a5bbdaafc870536ffc5986b34720018c6ac8fb05
KrishnaBalanagu <138356469+KrishnaBalanagu@users.noreply.github.com>
2023-07-08 14:46:50 +0530

    Time: 33 ms (73.13%), Space: 56.7 MB (75.28%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,46 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+       if(nums.length<=1) return nums.length;
         //TimeComplexity nLog(n)
-        if(nums.length<=1) return nums.length;
-        int k = 0;
-        int temp = 0;
-        TreeSet<Integer> sorted = new TreeSet<>();
+       //  int k = 0;
+       //  int temp = 0;
+       //  TreeSet<Integer> sorted = new TreeSet<>();
+       //  for(int i=0;i<nums.length;i++){
+       //      sorted.add(nums[i]);
+       //  }
+       // Iterator<Integer> iterator = sorted.iterator();
+       // int length = nums.length;
+       // int previous = iterator.next();
+       //  while(iterator.hasNext()){
+       //      int currentElement = iterator.next();
+       //      if(currentElement-previous !=1) {
+       //          k = Math.max(temp,k);
+       //          temp = 0;
+       //      }
+       //      else temp++;
+       //      previous = currentElement;
+       //  }
+       //  return Math.max(temp,k)+1;
+        //TimeComplexity O(n)
+       
+        Set<Integer> set = new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            sorted.add(nums[i]);
+            set.add(nums[i]);
         }
-       Iterator<Integer> iterator = sorted.iterator();
-       int length = nums.length;
-       int previous = iterator.next();
-        while(iterator.hasNext()){
-            int currentElement = iterator.next();
-            if(currentElement-previous !=1) {
-                k = Math.max(temp,k);
-                temp = 0;
-            }
-            else temp++;
-            previous = currentElement;
+        int longest = 0;
+        for(Integer element: set){
+            if(!set.contains(element-1)) longest = Math.max(checkForLongest(set,element),longest) ;
+            
         }
-        return Math.max(temp,k)+1;
+        return longest;
+        
+    }
+    public int checkForLongest(Set<Integer> set,int element){
+        int count = 1;
+        while(set.contains(element+1)){
+            count++;
+            element++;
+        }
+        return count;
     }
 }
